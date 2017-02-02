738d4c68e8228faca98ab639ad7d24e3dd6d55ff
김경남 <windfree@gimgyeongnam-ui-MacBook-Pro.local>
2017-02-02 00:20:30 -0800

    add app.go and other functions

--- agent/main/app.go
+++ agent/main/app.go
@@ -2,12 +2,52 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"sync"
+
+	"github.com/iwindfree/argos/agent/manager"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	command := os.Args[1]
+	switch command {
+	case "start":
+		start(&wg)
+	case "stop":
+		stop()
+	default:
+
+	}
+
+	manager.ServiceStart()
+	wg.Wait()
+	for {
+
+	}
+}
+
+func start(workGroup *sync.WaitGroup) {
 	displayLogo()
+	workGroup.Add(1)
+	displayLogo()
+	manager.ServiceStart()
+
+}
+
+func stop() {
+
 }
 
 func displayLogo() {
 	fmt.Println("MySQL Agent.")
+}
+
+func fileExist(filename string) bool {
+	if _, err := os.Stat(filename); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
