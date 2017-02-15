260f59d8fab8c813c651bf8ef355dd900e9bf533
김경남 <windfree@gimgyeongnam-ui-MacBook-Pro.local>
2017-02-15 01:10:08 -0800

    add configure

--- agent/config/configuration.go
+++ agent/config/configuration.go
@@ -7,6 +7,25 @@ import (
 type Configuraton struct {
 }
 
+type (
+	// for db instacne
+	dbInstance struct {
+		server   string `json:"db.server"`
+		port     string `json:"db.port"`
+		user     string `json:"db.user"`
+		password string `json:"db.password"`
+	}
+	// for configuration root
+	configure struct {
+		configureData struct {
+			collectorIp      string       `json:"scouter.server"`
+			collectorUdpPort string       `json:"scouter.server.udp.port"`
+			collectorTcpPort string       `json:"scouter.server.tcp.port"`
+			instances        []dbInstance `json:"instacnes"`
+		} `json:"configurations"`
+	}
+)
+
 var instance *Configuraton
 var once sync.Once
 
